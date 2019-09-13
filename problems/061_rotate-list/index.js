a77733e0eabd3387045520d540e1384bbde8daf5
Jason Zhang <jz@jzfed.com>
2019-09-13 16:22:10 +0800

    Update index.js

--- problems/061_rotate-list/index.js
+++ problems/061_rotate-list/index.js
@@ -13,21 +13,37 @@
  * @param {number} k
  * @return {ListNode}
  */
+// var rotateRight = function (head, k) {
+//     var nodes = [];
+//     var tmp = head;
+//     while (head) {
+//         nodes.push(head);
+//         head = head.next;
+//     }
+//     var l = nodes.length;
+//     k = k % l;
+//     if (k) {
+//         nodes[l - 1].next = nodes[0];
+//         nodes[l - k - 1].next = null;
+//         tmp = nodes[l - k];
+//     }
+//     return tmp;
+// };
+
 var rotateRight = function (head, k) {
-  var nodes = [];
-  var tmp = head;
-  while (head) {
-    nodes.push(head);
-    head = head.next;
-  }
-  var l = nodes.length;
-  k = k % l;
-  if (k) {
-    nodes[l - 1].next = nodes[0];
-    nodes[l - k - 1].next = null;
-    tmp = nodes[l - k];
-  }
-  return tmp;
+    while (k > 0) {
+        let last = head;
+        let prev;
+        while (last.next) {
+            prev = last;
+            last = last.next;
+        }
+        prev.next = null;
+        last.next = head;
+        head = last;
+        k--;
+    }
+    return head;
 };
 
 module.exports = rotateRight;
